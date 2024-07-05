494beb2d2498ec771ac0f03ae291c173db6a991c
Miguel Pena <miguel674@revature.net>
2024-07-05 01:08:35 -0700

    nestedlists added

--- NestedLists/main.py
+++ NestedLists/main.py
@@ -1,5 +1,11 @@
 if __name__ == "__main__":
+    records = []
     for _ in range(int(input())):
         name = input()
         score = float(input())
-        
+        records.append([name, score])
+
+    scores = sorted(set([record[1] for record in records]))
+    result = sorted([record[0] for record in records if record[1] == scores[1]])
+    for name in result:
+        print(name)
