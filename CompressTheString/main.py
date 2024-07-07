bc8abd846d7e98aae687b98ff7de9a252141eab6
Miguel Pena <miguel674@revature.net>
2024-07-07 14:03:57 -0700

    first attempt to solve

--- CompressTheString/main.py
+++ CompressTheString/main.py
@@ -1,4 +1,13 @@
 from itertools import groupby
 
 if __name__== '__main__':
-    pass
+    S = input()
+    result = []
+    for _, group in groupby(S):
+        group_list = list(group)
+        result.append((len(group_list), int(group_list[0])))
+    result_text = ''
+    for item in result:
+        result_text += str(item) + ' '
+
+    print(result_text)
