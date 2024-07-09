0f1b7d31e0825d47de708a550fd1b75425e44177
Miguel Pena <miguel674@revature.net>
2024-07-09 08:18:08 -0700

    first attempt

--- ElementRemoval/main.py
+++ ElementRemoval/main.py
@@ -1,5 +1,19 @@
 def elementRemoval(N, K, A):
-    return -404
+    dict1 = {}
+    count_removed = 0
+    
+    for elem in A:
+        if elem in dict1:
+            dict1[elem] += 1
+        else:
+            dict1[elem] = 1
+    
+    for key in dict1.keys():
+        result = dict1[key] % K
+        if result != 0:
+            count_removed += result
+            
+    return count_removed
 
 N = int(input())
 K = int(input())
