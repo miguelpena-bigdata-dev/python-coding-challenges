12099bb03015d92424bb100f2e58c2245a8bec55
Miguel Pena <miguel674@revature.net>
2024-07-09 07:53:09 -0700

    init

---
+++ ElementRemoval/main.py
@@ -0,0 +1,12 @@
+def elementRemoval(N, K, A):
+    return -404
+
+N = int(input())
+K = int(input())
+temp = input().split()
+A = []
+
+for i in range(N):
+    A.append(int(temp[i]))
+
+print(elementRemoval(N,K,A))
