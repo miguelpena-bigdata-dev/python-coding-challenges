a3b2f59531bd91fa30bdbc0002c59fbbb0aae05e
Miguel Pena <miguel674@revature.net>
2024-07-09 13:55:26 -0700

    completed

--- DesignerDoorMat/main.py
+++ DesignerDoorMat/main.py
@@ -1,3 +1,10 @@
 if __name__ == '__main__':
     n, m = map(int, input().split())
-    
+    design_one, design_two = '.|.', 'WELCOME'
+    top_half = [(design_one*i).center(m, '-') for i in range(1, (n // 2) * 2, 2)]
+    center_txt = design_two.center(m, '-')
+    bottom_half = [(design_one*i).center(m, '-') for i in reversed(range(1, (n // 2) * 2, 2))]
+
+    print('\n'.join(top_half))
+    print(center_txt)
+    print('\n'.join(bottom_half))
