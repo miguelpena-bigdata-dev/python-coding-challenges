f19df92c9584c6d7d81844db543848c253fe012f
Miguel Pena <miguel674@revature.net>
2024-07-17 14:44:55 -0700

    added findstring solution

--- findastring/main.py
+++ findastring/main.py
@@ -1,5 +1,15 @@
 def count_substring(string, sub_string):
-    pass
+    sub_string_length = len(sub_string)
+    if sub_string_length > len(string):
+        return 0
+    
+    count_value = 0
+    for start_value in range(0, len(string)):
+        current_substring = string[start_value:start_value + sub_string_length]
+        if current_substring == sub_string:
+            count_value += 1
+
+    return count_value
 
 if __name__ == '__main__':
     string = input().strip()
