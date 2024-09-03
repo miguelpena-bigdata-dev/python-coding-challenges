d1c649eecc637418383dff2e18fb410658b3562d
Miguel Pena <miguel674@revature.net>
2024-09-03 09:07:27 -0700

    using python features

--- RemoveElement/main.py
+++ RemoveElement/main.py
@@ -1,9 +1,20 @@
 class Solution:
     def removeElement(self, nums: list[int], val: int) -> int:
-        ...
+        
+        filtered_nums = list(filter(lambda x: x!=val, nums))
+        k = len(filtered_nums)
+        for i, num in enumerate(filtered_nums):
+            nums[i] = num
+            
+        return k        
+
 
 def main():
-    ...
+    nums = [3, 2, 2, 3]
+    val = 3
+    solution = Solution()
+    k = solution.removeElement(nums, val)
+    print(k)
 
 if __name__ == '__main__':
     main()
